91ec065a524f8ebd116f2cbab7abf15d1aa172cf
Wessel Loth <wessel@loth.io>
2023-10-04 19:57:57 +0200

    Add WIP ability to place a comment on the pull request that triggered the function

--- PullRequestCopilot/PullRequestUpdatedFunctions.cs
+++ PullRequestCopilot/PullRequestUpdatedFunctions.cs
@@ -7,6 +7,10 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
 using PullRequestCopilot.Models;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Web.Http;
 
 namespace PullRequestCopilot
 {
@@ -18,10 +22,10 @@ namespace PullRequestCopilot
             ILogger log)
         {
             log.LogInformation("Incoming HTTP trigger for PullRequestUpdated");
-
             string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
-            log.LogInformation("PullRequestUpdated RequestBody: {requestBody}", requestBody);
             var model = JsonConvert.DeserializeObject<PullRequestUpdatedModel>(requestBody);
+
+            log.LogInformation("PullRequestUpdated for PR #{PrId} RequestBody: {requestBody}", model.resource.pullRequestId, requestBody);
 
             if (model.resource.status == "completed")
             {
@@ -29,7 +33,38 @@ namespace PullRequestCopilot
                 return new OkResult();
             }
 
-            return new OkResult();
+            var adoPAT = System.Environment.GetEnvironmentVariable("AZUREDEVOPS_PAT", EnvironmentVariableTarget.Process);
+
+            // URL like: https://dev.azure.com/{organization}/{project}/_apis/git/repositories/{repository}/pullRequests/{pullRequestId}
+            var pullRequestUrl = model.resource.url;
+
+            var threadId = 1337;
+            var createThreadUrl = $"{pullRequestUrl}/threads/{threadId}/comments?api-version=7.0"; // https://learn.microsoft.com/en-us/rest/api/azure/devops/git/pull-request-thread-comments/create?view=azure-devops-rest-7.0&tabs=HTTP
+
+            // send post request to createThreadUrl
+            try
+            {
+                var client = new HttpClient();
+                var values = new Dictionary<string, string>
+                  {
+                      { "content", "Hello World from Azure Functions!" },
+                  };
+
+                var content = new FormUrlEncodedContent(values);
+                content.Headers.TryAddWithoutValidation("Authorization", $"Basic {adoPAT}");
+
+                var response = await client.PostAsync(createThreadUrl, content);
+
+                var responseString = await response.Content.ReadAsStringAsync();
+                log.LogInformation("PullRequestUpdated Azure DevOps Response: {responseBody}", responseString);
+
+                return new OkResult();
+            }
+            catch (Exception e)
+            {
+                log.LogError(e.Message, e);
+                return new InternalServerErrorResult();
+            }
         }
     }
 }
--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # ado-pr-copilot
+
+## TL;DR
+- Analyzes your Azure DevOps Pull Request automatically as they are created/updated
+- Provides AI-backed suggestions as comments on your PR!
+
+## TODO
+- Actually analyzing the contents using OpenAI
+- Being able to have a "system" user provide these suggestions
+- Being able to easily install this as an extension for Azure DevOps
+
+## Installation
+- Deploy this solution to your Function App
+- Set up a new Service Hook in your Azure DevOps project
+- Select "Web Hooks"
+- Select the "Pull Request Updated" trigger type
+- Point the url to your function app endpoint
+- No additional configuration is necessary (testing will not work as the function tries to connect back to the fake URL in the test message)
+- Save & enjoy!
+
+## Development
+- A function app to deploy to
+- The `AZUREDEVOPS_PAT` environment variable with a Personal Access Token configured (read scopes)
