ce56d1b186efb689d0d8625e1dc1a5c5599e5e74
Wessel Loth <wessel@loth.io>
2023-10-04 19:00:58 +0200

    Add simple function that accepts an Azure DevOps webhook and logs the body

---
+++ PullRequestCopilot/PullRequestUpdatedFunctions.cs
@@ -0,0 +1,26 @@
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+
+namespace PullRequestCopilot
+{
+    public static class PullRequestUpdatedFunctions
+    {
+        [FunctionName("PullRequestUpdated")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "post")] HttpRequest req,
+            ILogger log)
+        {
+            log.LogInformation("C# HTTP trigger function processed a request.");
+
+            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+            log.LogInformation("RequestBody: {requestBody}", requestBody);
+
+            return new OkResult();
+        }
+    }
+}
